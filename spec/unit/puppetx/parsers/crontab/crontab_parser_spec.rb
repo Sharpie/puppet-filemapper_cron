279b027bfca7ac09a49791650325d76c92f8d4fc
Charlie Sharpsteen <source@sharpsteen.net>
2014-03-15 12:41:47 -0700

    CrontabParser: Spec test the schedule parsers
    Added some spec tests that cover the parsing of normal and special schedules.
    There are already some failuers as the parsers are currently matching 0 or more
    characters in places where 1 or more is required.

--- spec/unit/puppetx/parsers/crontab/crontab_parser_spec.rb
+++ spec/unit/puppetx/parsers/crontab/crontab_parser_spec.rb
@@ -1,10 +1,51 @@
 require 'spec_helper'
+require 'parslet/rig/rspec'
 require 'puppetx/parsers/crontab/crontab_parser'
 
 describe PuppetX::Parsers::Crontab::CrontabParser do
   let(:simple_crontab) { File.read(my_fixture('simple_crontab')) }
 
-  it 'parses a simple crontab' do
-    expect{ subject.parse(simple_crontab) }.to_not raise_error
+  describe 'when parsing schedules' do
+
+    context 'and the schedule is special' do
+      it 'should consume input beginning with @' do
+        expect(subject.special).to parse('@schedule').as({:special => '@schedule'})
+      end
+
+      it 'should require input to follow @' do
+        expect(subject.special).to_not parse('@')
+      end
+    end
+
+    context 'and the schedule is normal' do
+      it 'should consume five space delimited groups' do
+        expect(subject.schedule).to parse('0,15,30,45 8-18,20-22 31 12 7').as({
+          :minute   => '0,15,30,45',
+          :hour     => '8-18,20-22',
+          :monthday => '31',
+          :month    => '12',
+          :weekday  => '7'
+        })
+      end
+
+      it 'should require space between groups' do
+        expect(subject.schedule).to_not parse('*****')
+      end
+
+      it 'should consume extra whitespace between groups' do
+        expect(subject.schedule).to parse('*  *  *  *  *')
+        expect(subject.schedule).to parse("*\t* *\t* *")
+      end
+
+      it 'should not consume less than five groups' do
+        expect(subject.schedule).to_not parse('* * * *')
+      end
+
+      it 'should not consume more than five groups' do
+        expect(subject.schedule).to_not parse('* * * * * *')
+      end
+    end
+
   end
+
 end
